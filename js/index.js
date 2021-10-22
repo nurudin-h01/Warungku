e9d2a7322d5f1e071291c7eb603dc68fe111d8b1
Jessica Wong <wongjessica124@gmail.com>
2021-10-22 12:27:33 +0700

    Finish login feature and some homepage layout

--- js/index.js
+++ js/index.js
@@ -1,1 +1,25 @@
 // 
+
+// IMPORT FROM helpers.js
+import { isLogin, logout } from "./helpers.js";
+
+const loginBtnEl = document.getElementById("loginBtn")
+const logoutBtnEl = document.getElementById("logoutBtn")
+
+const checkLogin = () => {
+  if(isLogin()) {
+    loginBtnEl.classList.add('d-none')
+    logoutBtnEl.classList.remove('d-none')
+  }
+}
+
+window.onload = checkLogin()
+
+loginBtnEl.addEventListener('click', () => {window.location.href = "../login.html"})
+
+logoutBtnEl.addEventListener('click', () => {
+  let isTrue = confirm("Anda yakin ingin logout?");
+  if(isTrue) {
+    logout()
+  }
+})
