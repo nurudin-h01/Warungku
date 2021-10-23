060cc8fa296d64afa0e6203a06b9cda07e1080fd
Jessica Wong <wongjessica124@gmail.com>
2021-10-23 09:15:46 +0700

    Homepage layout finish

--- js/index.js
+++ js/index.js
@@ -1,25 +1,58 @@
 // 
 
 // IMPORT FROM helpers.js
-import { isLogin, logout } from "./helpers.js";
+import { isLogin, logout, getUser, getProducts } from "./helpers.js";
 
 const loginBtnEl = document.getElementById("loginBtn")
-const logoutBtnEl = document.getElementById("logoutBtn")
+const logoutBtnEl = document.getElementById("confirmLogout")
+const accountMenuEl = document.getElementById("account-menu")
+const belanjaBtnEl = document.querySelectorAll(".belanja")
+const beliBtnEl = document.querySelectorAll(".beli")
+const modalEl = new bootstrap.Modal(document.getElementById('shouldLoginModal'), {
+  keyboard: false
+})
 
 const checkLogin = () => {
   if(isLogin()) {
+    let user = getUser()
     loginBtnEl.classList.add('d-none')
-    logoutBtnEl.classList.remove('d-none')
+    accountMenuEl.classList.remove('d-none')
   }
 }
 
 window.onload = checkLogin()
 
+// LOGIN BUTTON CLICK
 loginBtnEl.addEventListener('click', () => {window.location.href = "../login.html"})
 
+// LOGOUT
 logoutBtnEl.addEventListener('click', () => {
-  let isTrue = confirm("Anda yakin ingin logout?");
-  if(isTrue) {
-    logout()
-  }
+  logout()
 })
+
+belanjaBtnEl.forEach(belanjaBtn => {
+  belanjaBtn.addEventListener('click', () => {
+    if(!isLogin()) {
+      modalEl.toggle()
+    }
+  })
+})
+
+beliBtnEl.forEach(beliBtn => {
+  beliBtn.addEventListener('click', () => {
+    if(!isLogin()) {
+      modalEl.toggle()
+    }
+  })
+})
+
+const renderproducts = async () => {
+  // EDIT HERE
+  let products = await getProducts()
+  products.map(async (value) => {
+    console.log(value);
+  })
+  
+};
+
+renderproducts()
