96274f890668b8143ca2097edb3ab36ca596ae6b
Jessica Wong <wongjessica124@gmail.com>
2021-10-24 23:08:05 +0700

    Display new product from fetch api

--- js/index.js
+++ js/index.js
@@ -7,10 +7,10 @@ const loginBtnEl = document.getElementById("loginBtn")
 const logoutBtnEl = document.getElementById("confirmLogout")
 const accountMenuEl = document.getElementById("account-menu")
 const belanjaBtnEl = document.querySelectorAll(".belanja")
-const beliBtnEl = document.querySelectorAll(".beli")
 const modalEl = new bootstrap.Modal(document.getElementById('shouldLoginModal'), {
   keyboard: false
 })
+const newProdductListEl = document.getElementById("newProductList")
 
 const checkLogin = () => {
   if(isLogin()) {
@@ -38,21 +38,47 @@ belanjaBtnEl.forEach(belanjaBtn => {
   })
 })
 
-beliBtnEl.forEach(beliBtn => {
-  beliBtn.addEventListener('click', () => {
-    if(!isLogin()) {
-      modalEl.toggle()
-    }
-  })
-})
+
+// RENDER PRODUCT TO INDEX
+const createProductElement = (product) => {
+  const colEl = document.createElement('div')
+  colEl.classList.add("col-lg-3")
+  
+  const productCardEl = `
+  <div class="card">
+    <div class="card-body text-center">
+      <small class="d-none" id="idProduct" data-id=${product.id}></small>
+      <img src="${product.img}" class="card-img-top rounded-3" alt="Tomatoes">
+      <h5 class="card-title text-center mt-3 mb-2">${product.name}</h5>
+      <h6 class="mb-2">Rp${new Intl.NumberFormat("id-ID").format(product.harga)}</h6>
+      <p class="text-primary"><small><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="far fa-star"></i></small></p>
+      <a class="btn btn-sm btn-success beli">Beli</a>
+    </div>
+  </div>
+    `
+
+  colEl.insertAdjacentHTML('beforeend', productCardEl)
+  newProdductListEl.append(colEl)
+}
 
 const renderproducts = async () => {
   // EDIT HERE
   let products = await getProducts()
-  products.map(async (value) => {
-    console.log(value);
+  products.slice(-5,-1).map(async (product) => {
+    createProductElement(product);
   })
   
+  const beliBtnEl = document.querySelectorAll(".beli")
+  
+  beliBtnEl.forEach(beliBtn => {
+    beliBtn.addEventListener('click', () => {
+      if(!isLogin()) {
+        modalEl.toggle()
+      }
+    })
+  })
+
 };
 
 renderproducts()
+  
